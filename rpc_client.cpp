1d982230c76d1472ad62a68dad054ea43ed59730
Mark Veinot <mveinot@gmail.com>
2023-06-20 21:57:38 -0300

    Code cleanup

--- rpc_client.cpp
+++ rpc_client.cpp
@@ -21,11 +21,27 @@ void rpc_client::init()
 
 void rpc_client::replyFinished(QNetworkReply * reply)
 {
-    QByteArray _token = reply->rawHeader("X-Transmission-Session-Id");
-    //qDebug() << "debug: " << _token;
-    rpc_client::setSessionToken(_token);
-    //reply->deleteLater(); // make sure to clean up
-    //getTorrentList();
+    //if(reply->error() == QNetworkReply::NoError){
+        qDebug() << "Network reply OK";
+    qDebug() << reply->error();
+        if (_session_token.isEmpty())
+        {
+            QByteArray _token = reply->rawHeader("X-Transmission-Session-Id");
+            rpc_client::setSessionToken(_token);
+        } else {
+            QString contents = QString::fromUtf8(reply->readAll());
+            QJsonDocument doc = QJsonDocument::fromJson(contents.toUtf8());
+            QJsonValue dObj = doc["arguments"];
+            QJsonValue torrentsObj = dObj["torrents"];
+            rpc_client::torrentList = torrentsObj.toArray();
+        }
+        /*} else {
+        QString err = reply->errorString();
+        QString contents = QString::fromUtf8(reply->readAll());
+        qDebug() << "Network reply ERROR";
+        qDebug() << err;
+        qDebug() << contents;
+    }*/
 }
 
 bool rpc_client::isClientReady()
@@ -42,35 +58,24 @@ void rpc_client::setSessionToken(QByteArray token)
 
 void rpc_client::getTorrentList()
 {
-    //"{\"method\":\"torrent-get\",\"arguments\": {\"fields\":[\"rateDownload\",\"rateUpload\",\"id\",\"percentDone\",\"status\",\"name\"]}}"
     QNetworkRequest request = QNetworkRequest(QUrl("http://nas2.mvgrafx.net:9091/transmission/rpc"));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     request.setRawHeader(QByteArray("X-Transmission-Session-Id"), QByteArray(rpc_client::_session_token));
     qDebug() << _session_token;
 
     QByteArray data("{\"method\":\"torrent-get\",\"arguments\": {\"fields\":[\"rateDownload\",\"rateUpload\",\"id\",\"percentDone\",\"status\",\"name\"]}}");
-    QNetworkReply *reply = na_manager->post(request, data);
+    //QNetworkReply *reply =
+    na_manager->post(request, data);
 
+    /*
     QObject::connect(reply, &QNetworkReply::finished, [=](){
         if(reply->error() == QNetworkReply::NoError){
             qDebug() << "OK";
             QString contents = QString::fromUtf8(reply->readAll());
             QJsonDocument doc = QJsonDocument::fromJson(contents.toUtf8());
-            //QJsonObject obj = doc.object();
             QJsonValue dObj = doc["arguments"];
             QJsonValue torrentsObj = dObj["torrents"];
-            //QJsonArray torrentsArray = torrentsObj.toArray();
             rpc_client::torrentList = torrentsObj.toArray();
-            //["torrents"].toArray();
-            //qDebug() << obj;
-            //qDebug() << torrentsArray;
-            // Iterate over the array and print each value
-            /*
-            for (const QJsonValueRef item : torrentsArray)
-            {
-                qDebug()<< item.toObject().value("name").toString();
-            }
-            */
         }
         else{
             QString err = reply->errorString();
@@ -81,4 +86,5 @@ void rpc_client::getTorrentList()
         }
         reply->deleteLater();
     });
+    */
 }
