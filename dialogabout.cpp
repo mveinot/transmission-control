2c5dca27c3e68118bad253943f638c2f3ccd9c51
Mark Veinot <vic@mvgrafx.net>
2026-06-16 21:55:15 -0300

    Move version generation to Cmake

--- dialogabout.cpp
+++ dialogabout.cpp
@@ -45,7 +45,7 @@ DialogAbout::DialogAbout(QWidget *parent)
                                "<p>Version: %1</p>"
                                "<p>Build date: %2</p>"
                                "<p>Copyright © Mark Veinot</p>"
-                               ).arg(QCoreApplication::applicationVersion(), QStringLiteral(__BUILD_DATE__)));
+                               ).arg(QCoreApplication::applicationVersion(), QStringLiteral(__DATE__)));
 
     const QString creditsPath =
         bundledResourcePath("licenses/THIRD_PARTY_NOTICES.txt");
